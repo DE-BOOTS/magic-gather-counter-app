af12b81b0f9739772c1e0ac8bcec9a703d818077
Nkuna <123546993+DE-BOOTS@users.noreply.github.com>
2023-12-27 08:24:58 +0200

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,87 @@
+<script>
+  import Team from './lib/Team.svelte'
+
+  //variable declaration
+  let red = 20
+  let blue = 20
+  let redTeam = ' red'
+  let blueTeam = 'blue'
+  let teamR = 'Red'
+  let teamB = 'Blue'
+
+  //reactive value declaration
+  $: redPoints = red
+  $: bluePoints = blue
+
+  //function handlers
+
+  function redHandler() {
+    red += 1
+  }
+
+  function redSubHandler() {
+    red -= 1
+  }
+
+  function bluePlusHandler() {
+    blue += 1
+  }
+
+  function blueSubHandler() {
+    blue -= 1
+  }
+
+  ///reset function handler
+
+  function resetHandler() {
+    redPoints = 20
+    bluePoints = 20
+  }
+</script>
+
+<header>MTG Counter App</header>
+<main>
+  <Team
+    increaseHandler={redHandler}
+    decreaseHandler={redSubHandler}
+    teamName={teamR}
+    number={redPoints}
+    teamColor={redTeam}
+  />
+  <Team
+    increaseHandler={bluePlusHandler}
+    decreaseHandler={blueSubHandler}
+    teamName={teamB}
+    number={bluePoints}
+    teamColor={blueTeam}
+  />
+</main>
+<div class="container">
+  <button class="btn-reset" on:click={resetHandler}>Reset Game</button>
+</div>
+
+<style>
+  header {
+    margin: 20px 0;
+    text-align: center;
+    font-weight: 900;
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+  }
+  main {
+    display: flex;
+    flex: 1;
+  }
+  .container {
+    position: absolute;
+    left: 10%;
+  }
+
+  .container button {
+    padding: 4px;
+    width: 100%;
+    font-weight: 900;
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+    background-color: orange;
+    border: 1px solid grey;
+  }
+</style>
---
+++ src/lib/Team.svelte
@@ -0,0 +1,79 @@
+<script>
+  export let teamName = 'Name'
+  export let number = 20
+  export let teamColor = 'black'
+  export let increaseHandler
+  export let decreaseHandler
+  let disable = false
+
+  function addHandler() {
+    increaseHandler()
+  }
+
+  function subtractHandler() {
+    decreaseHandler()
+  }
+</script>
+
+<div class="row" style="color:{teamColor}">
+  <h4>Team {teamName}: <span>{number}</span></h4>
+  <div class="column">
+    <button on:click={addHandler} class="btn btn-1">+</button>
+    <button on:click={subtractHandler} class="btn btn-2">-</button>
+  </div>
+  {#if number === 0}
+    <h5>{teamName} team is the winner</h5>
+    <style>
+      .btn {
+        pointer-events: none;
+      }
+    </style>
+    {#if teamName === 'Red'}
+      <style>
+        .row {
+          color: rgba(255, 0, 0, 0.2);
+        }
+      </style>
+    {:else if teamName === 'Blue'}
+      <style>
+        .row {
+          color:.row {
+            color: rgba(0, 0, 255, 0.2);
+          }
+        }
+      </style>
+    {/if}
+  {/if}
+</div>
+
+<style>
+  .row {
+    margin: 20px 5px;
+    border: 2px dashed;
+    width: 40%;
+    padding-bottom: 10px;
+    display: flex;
+    flex: 1;
+  }
+  h5 {
+    text-align: center;
+    margin-top: 5px;
+  }
+
+  h4 {
+    text-align: center;
+    font-weight: bold;
+  }
+  .column {
+    text-align: center;
+    align-items: center;
+  }
+  .btn-1 {
+    background-color: green;
+    color: white;
+  }
+  .btn-2 {
+    background-color: red;
+    color: white;
+  }
+</style>
